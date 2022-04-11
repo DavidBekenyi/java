6dad0179ba2a78e0514d49332c4606e9463843b3
DavidBekenyi <90574330+DavidBekenyi@users.noreply.github.com>
2022-04-11 10:40:07 +0200

    Update kosar.js

--- kosar.js
+++ kosar.js
@@ -1,1 +1,13 @@
+var jatek01 = 15500
+var jatek02 = 6999
+var jatek03 = 2980
 
+
+function hozzaad(jatek) {
+    db=Number(prompt("db:"))
+    localStorage.setItem(jatek,db)
+}
+
+function torles(jatek,db){
+localStorage.clear()
+}
